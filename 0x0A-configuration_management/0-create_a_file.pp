a6a82ea72ddd729d7ec22038371553eb8d0210ff
Mariam Moustafa <mariam.moustafa57@gmail.com>
2024-04-19 10:54:33 -0700

    Configuration Management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#The code will create a file named school in the /tmp directory
+file { '/tmp/school':
+  ensure => file,
+  content => 'I love Puppet',
+  mode  => '07744',
+  owner => 'www-data',
+  group => 'www-data'
+}
